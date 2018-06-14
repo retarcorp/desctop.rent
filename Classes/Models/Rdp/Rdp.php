b78bc429cbcc1ce94c3b2bd4fedaf6b8f5b915da
Denis Latushkin <info@retarcorp.by>
2018-06-14 13:38:44 +0000

    changed Rdp.php. class rdp returns select query and method getContent returns content field

--- Classes/Models/Rdp/Rdp.php
+++ Classes/Models/Rdp/Rdp.php
@@ -4,25 +4,24 @@ namespace Classes\Models\Rdp;
 use Classes\Utils\Sql;
 
 class Rdp{
+    const TABLE_NAME = "rdp_connections";
     public $id, $ip, $created_at, $due_to, $uid ;
-    private $content; 
     public function __construct(int $id){
         $sql = Sql::getInstance(); 
-        # @TODO fill this rdp from database by given id
-        $r = $sql->getAssocArray("SELECT id, ip, created_at, due_to, uid, FROM rdp_connections WHERE id=$id"); //\\
+        $r = $sql->getAssocArray("SELECT ip, created_at, due_to, uid FROM ".self::TABLE_NAME." WHERE id=$id");
         $r = $r[0];
-        $this->id = $r['id'];
+        $this->id = $id;
         $this->ip = $r['ip'];
-        $this->uid = $r['uid'];
         $this->created_at = $r['created_at'];
         $this->due_to = $r['due_to'];
+        $this->uid = $r['uid'];
     }
 
     public function getContent(){
-        # @TODO get text contents of this rdp from database
-        $r = $sql->getAssocArray("SELECT content FROM rdp_connections WHERE id=$id");
+        $sql = Sql::getInstance();
+        $id = $this->id;
+        $r = $sql->getAssocArray("SELECT content FROM ".self::TABLE_NAME." WHERE id=$id");
         $r = $r[0];
-        $this->content = $r['content'];
-        return $this->content; 
+        return $r['content'];
     }
 }
